2cf89709a4bbfcc7b85133acd402792d7b9a8053
井上沙耶 <>
2018-07-17 11:26:58 +0800

    ユーザー名が空だった時にエラーメッセージが出る

---
+++ registsar/signup.php
@@ -0,0 +1,63 @@
+<?php
+    $errors = [];
+
+    // 空のものがあるとポスト送信しない
+    if(!empty($_POST)){
+        $name = $_POST['input_name'];
+
+        // ユーザー名の空チェック
+        if($name == ''){
+            $errors['name'] = 'blank';
+
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>Learn SNS</title>
+        <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
+    </head>
+    <body style="margin-top: 60px">
+        <div class="container">
+            <div class="row">
+                <!-- ここにコンテンツ -->
+
+      <!-- ここから -->
+      <div class="col-xs-8 col-xs-offset-2 thumbnail">
+        <h2 class="text-center content_header">アカウント作成</h2>
+        <form method="POST" action="signup.php" enctype="multipart/form-data">
+          <div class="form-group">
+            <label for="name">ユーザー名</label>
+            <input type="text" name="input_name" class="form-control" id="name" placeholder="山田 太郎">
+            <?php if (isset($errors['name']) && $errors['name'] == 'blank'): ?>
+                <p class="text-danger">ユーザー名を入力してください。</p>
+            <?php endif; ?>
+          </div>
+
+          <div class="form-group">
+            <label for="email">メールアドレス</label>
+            <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com">
+          </div>
+          <div class="form-group">
+            <label for="password">パスワード</label>
+            <input type="password" name="input_password" class="form-control" id="password" placeholder="4 ~ 16文字のパスワード">
+          </div>
+          <div class="form-group">
+            <label for="img_name">プロフィール画像</label>
+            <input type="file" name="input_img_name" id="img_name">
+          </div>
+          <input type="submit" class="btn btn-default" value="確認">
+          <a href="../signin.php" style="float: right; padding-top: 6px;" class="text-success">サインイン</a>
+        </form>
+      </div>
+      <!-- ここまで -->
+            </div>
+        </div>
+        <script src="../assets/js/jquery-3.1.1.js"></script>
+        <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
+        <script src="../assets/js/bootstrap.js"></script>
+    </body>
+</html>
