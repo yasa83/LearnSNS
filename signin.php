f72c393ab8f53e0e5544990fa6727be2f16cb749
井上沙耶 <>
2018-07-20 10:13:14 +0800

    email,passwordが空だったらエラーを出すphp

--- signin.php
+++ signin.php
@@ -1,5 +1,19 @@
 <?php
-// サインイン処理
+// 初期化
+    $errors = [];
+
+    if(!empty($_POST)){
+        $email = $_POST['input_email'];
+        $password = $_POST['input_password'];
+
+        if($email!=''&& $password!=''){
+            //データベースとの照合処理
+        }else{
+            $errors['signin'] ='blank';
+        }
+
+        echo "てすと";
+    }
 ?>
 <!DOCTYPE html>
 <html lang="ja">
