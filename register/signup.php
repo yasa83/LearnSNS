a831bc007c18af464b1e5789a40873425c21eb5a
井上沙耶 <>
2018-07-25 22:49:32 +0800

    【004】サインイン画面を作成

--- register/signup.php
+++ register/signup.php
@@ -5,13 +5,43 @@
     <title>LearnSNS</title>
     <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">   
     <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
 </head>
 <body style="margin-top:60px">
     <div class="container">
         <div class="row">
         
+        <div class="col-xs-8 col-xs-offset-2 thumbnail">
+            <h2 class="text-center count_header">アカウント作成</h2>
+            <form method="POST" action="signup.php" enctype="multipart/form-data">
+
+                <div class="form-group">
+                    <label for="name">ユーザー名</label>
+                    <input type="text" name="input_name" class="form-control" id="name" placeholder="山田太郎">
+                </div>
+
+                <div class="form-group">
+                    <label for="email">メールアドレス</label>
+                    <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">パスワード</label>
+                    <input type="password" name="input_password" class="form-control" id="password" placeholder="4 ~ 16文字のパスワード">
+                </div>
+
+                <div class="form-group">
+                    <label for="img_name">プロフィール画像</label>
+                    <input type="file" name="input_img_name" id="img_name">
+                </div>
+
+                <input type="submit" class="btn btn-default" value="確認">
+                <a href="../signin.php" style="float: right; padding-top: 6px;" class="text-success">サインイン</a>
+            </form>
+        </div>
         </div>
     </div>
+    
     <script src="../assets/js/jquery-3.1.1.js"></script>
     <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
     <script src="../assets/js/bootstrap.js"></script>
