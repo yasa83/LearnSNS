0029217b5ddc25b7cade4f911614d56c18bc03fe
井上沙耶 <>
2018-07-28 09:07:54 +0800

    検索機能を実装

--- timeline.php
+++ timeline.php
@@ -109,6 +109,31 @@ if ($feed != '') {
         $feeds[] =$record;
     }
     
+    // 検索機能
+    if(isset($_GET['search_word'])){
+        $sql ='SELECT `f`.*, `u`.`name`,`u`.`img_name` FROM `feeds` AS `f` LEFT JOIN `users` AS `u` ON `f`.`user_id`=`u`.`id` WHERE f.feed LIKE "%"?"%" ORDER BY `created` DESC LIMIT '. CONTENT_PER_PAGE.' OFFSET '.$start;
+        $data = [$_GET['search_word']];
+    }else{
+        // LEFT JOINで全件取得
+        $sql = 'SELECT `f`.*, `u`.`name`, `u`.`img_name` FROM `feeds` AS `f` LEFT JOIN `users` AS `u` ON `f`.`user_id`=`u`.`id` ORDER BY `created` DESC LIMIT '.CONTENT_PER_PAGE.'OFFSET'.$start;
+                $data=[];
+    }
+
+        
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute($data);
+
+
+    // 表示用の配列を初期化
+        $feeds=array();
+
+        while(true){
+        $record = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($record == false){
+            break;
+        }
+        $feeds[] =$record;
+        }
 
     $dbh = null;
 
