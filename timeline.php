2828f28c73c6c850d80a81ab3fb92a2fb1f2244b
井上沙耶 <>
2018-07-30 11:00:23 +0800

    いいね!ボタンを押したらファイルステータス、コンソールにボタンが押されましたと表示される。

--- timeline.php
+++ timeline.php
@@ -265,5 +265,7 @@ if ($feed != '') {
     <script src="assets/js/jquery-3.1.1.js"></script>
     <script src="assets/js/jquery-migrate-1.4.1.js"></script>
     <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/app.js"></script>
+
 </body>
 </html>
