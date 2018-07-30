951ba5b5dfe627e787c7f4bb15135ece94d7931d
井上沙耶 <>
2018-07-30 11:21:07 +0800

    いいねを押した時のユーザーのIDを取得

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,17 @@
 $(function() {
 
     $('.js-like').on('click', function() {
-        console.log('ボタンが押されました。');
+        // console.log('ボタンが押されました。');
+        $.ajax({
+            // 送信先、送信するデータなど
+        })
+        .done(function(data) {
+            // 成功時の処理
+        })
+        .fail(function(err) {
+            // 失敗時の処理
+        })
     });
+
 
 });
