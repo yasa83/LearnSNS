e092b3742118f94ecf888afe61075797391f5a11
井上沙耶 <>
2018-07-30 11:30:51 +0800

    いいねのときユーザーIDの取得

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,13 @@ $(function() {
 
     $('.js-like').on('click', function() {
         // console.log('ボタンが押されました。');
+        var feed_id = $(this).siblings('.feed-id').text();
+        var user_id = $('#signin-user').text();
+        console.log(feed_id);   //feed_idを取得できているか確認
+        console.log(user_id);   //user_idを取得できているか確認
+
+
+
         $.ajax({
             // 送信先、送信するデータなど
         })
