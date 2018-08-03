789128c609884b64f2137f922ae6e87f25ed12b4
井上沙耶 <>
2018-08-03 09:33:20 +0800

    投稿処理を関数化

--- function.php
+++ function.php
@@ -1,6 +1,6 @@
 <?PHP
 
-    //  SQLから配列を受け取る
+    //  SQLからユーザー情報の配列を受け取る
     function get_user($dbh,$user_id)
     {
         $sql = 'SELECT * FROM `users` WHERE `id`=?';
@@ -9,4 +9,13 @@
         $stmt->execute($data);
 
         return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    // 投稿処理を関数化
+    function create_feed($dbh,$feed,$user_id)
+    {
+    $sql = 'INSERT INTO `feeds` SET `feed`=?, `user_id`=?, `created`=NOW()';
+    $data = array($feed, $user_id);
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
     }
