89e6ab4bc0bb7aafb9272cc0a39be29d117c236f
井上沙耶 <>
2018-08-03 09:41:04 +0800

    いいねの件数をリファクタリング

--- function.php
+++ function.php
@@ -14,8 +14,24 @@
     // 投稿処理を関数化
     function create_feed($dbh,$feed,$user_id)
     {
-    $sql = 'INSERT INTO `feeds` SET `feed`=?, `user_id`=?, `created`=NOW()';
-    $data = array($feed, $user_id);
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute($data);
+        $sql = 'INSERT INTO `feeds` SET `feed`=?, `user_id`=?, `created`=NOW()';
+        $data = array($feed, $user_id);
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute($data);
+    }
+
+
+    // 何件いいねされているか確認する機能を関数化
+    function count_like($dbh,$feed_id)
+    {
+        $like_sql = "SELECT COUNT(*) AS `like_cnt` FROM `likes` WHERE `feed_id` = ?";
+
+        $like_data = [$feed_id];
+
+        $like_stmt = $dbh->prepare($like_sql);
+        $like_stmt->execute($like_data);
+
+        $like = $like_stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $like["like_cnt"];
     }
