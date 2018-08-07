d74abb30d265a2c80a4dc782cf03eae1c4ea2e22
井上沙耶 <>
2018-08-07 11:18:23 +0800

    ユーザー一覧にリンクはる

--- timeline.php
+++ timeline.php
@@ -145,7 +145,7 @@ if ($feed != '') {
             <div class="collapse navbar-collapse" id="navbar-collapse1">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="#">タイムライン</a></li>
-                    <li><a href="#">ユーザー一覧</a></li>
+                    <li><a href="users.php">ユーザー一覧</a></li>
                 </ul>
                 <form method="GET" action="" class="navbar-form navbar-left" role="search">
                     <div class="form-group">
