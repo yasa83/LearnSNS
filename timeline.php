42f14e053d064c03b7d057bc13209ecf423c8e0f
井上沙耶 <>
2018-08-07 21:03:10 +0800

    ユーザー一覧ページ作成完了

--- timeline.php
+++ timeline.php
@@ -131,43 +131,8 @@ if ($feed != '') {
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body style="margin-top: 60px; background: #E4E6EB;">
-    <div class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Learn SNS</a>
-            </div>
-            <div class="collapse navbar-collapse" id="navbar-collapse1">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">タイムライン</a></li>
-                    <li><a href="users.php">ユーザー一覧</a></li>
-                </ul>
-                <form method="GET" action="" class="navbar-form navbar-left" role="search">
-                    <div class="form-group">
-                        <input type="text" name="search_word" class="form-control" placeholder="投稿を検索">
-                    </div>
-                    <button type="submit" class="btn btn-default">検索</button>
-                </form>
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                        <!-- いいねしたときユーザーID取得 -->
-                        <span hidden id="signin-user"><?php echo $signin_user['id']; ?></span>
 
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="user_profile_img/<?php echo $signin_user['img_name']; ?>" width="18" class="img-circle"><?php echo $signin_user['name']; ?> <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">マイページ</a></li>
-                            <li><a href="signout.php">サインアウト</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
+<?php include('navbar.php');?>
 
     <div class="container">
         <div class="row">
