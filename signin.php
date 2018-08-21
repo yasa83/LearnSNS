e6db92d7d239a9ac40504d67409e46d3383f3ac1
井上沙耶 <>
2018-08-22 07:22:01 +0800

    7.メールアドレスの空のバリデーション実装

--- signin.php
+++ signin.php
@@ -1,7 +1,18 @@
 <?php
+$errors = [];
 
+if(!empty($_POST)){
+    
+    $email = $_POST['input_email'];
+    $password = $_POST['input_password'];
+
+    if($email !='' && $password !=''){
+        //データベースとの照合処理
+    }else{
+        $errors['signin'] = 'blank';
+    }
+}
 ?>
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -16,10 +27,13 @@
         <div class="row">
             <div class="col-xs-8 col-xs-offset-2 thumbnail">
                 <h2 class="text-center content_header">サインイン</h2>
-                <form method="POST" action="" enctype="multipart/form-data">
+                <form method="POST" action="signin.php" enctype="multipart/form-data">
                     <div class="form-group">
                         <label for="email">メールアドレス</label>
                         <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com">
+                        <?php if(isset($errors['signin']) && $errors['signin'] == 'blank'): ?>
+                            <p class="text-danger">メールアドレスとパスワードを正しく入力してください</p>
+                        <?php endif; ?>
                     </div>
                     <div class="form-group">
                         <label for="password">パスワード</label>
