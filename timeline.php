f7564b4ba3586ae701df40db061d12c46c0c8064
井上沙耶 <>
2018-09-07 22:26:37 +0800

    12 いいね機能実装　HTMLをしゅうせい

--- timeline.php
+++ timeline.php
@@ -175,13 +175,12 @@ while(true){
                     </div>
                     <div class="row feed_sub">
                         <div class="col-xs-12">
-                            <form method="POST" action="" style="display: inline;">
-                                <input type="hidden" name="feed_id" >
-
-                                <input type="hidden" name="like" value="like">
-                                <button type="submit" class="btn btn-default btn-xs"><i class="fa fa-thumbs-up" aria-hidden="true"></i>いいね！</button>
-                            </form>
-                            <span class="like_count">いいね数 : 100</span>
+                            <span hidden><?=$feed['id']?></span>
+                                <button class="btn btn-default btn-xs js-like">
+                                    <i class="fa fa-thumbs-up" aria-hidden="true"></i>
+                                    <span>いいね！</span> 
+                                </button>
+                                <span class="like_count">100</span>
                             <span class="comment_count">コメント数 : 9</span>
                             <?php if($feed['user_id'] == $_SESSION['id']): ?>
                             <a href="edit.php?feed_id=<?php echo $feed['id']; ?>" class="btn btn-success btn-xs">編集</a>
