068563ba22919f1d430d085d65207f8be86467c0
井上沙耶 <>
2018-09-13 23:37:56 +0800

    いいね機能の途中

--- like.php
+++ like.php
@@ -1,5 +1,16 @@
 <?php
 
-echo json_encode('test');
+require_once('debconnect.php');
+
+$feed_id = $_POST['feed_id'];
+$user_id = $_POST['user_id'];
+
+$sql = 'INSERT INTO `likes` (`user_id`,`feed_id`) VALUES(?,?);';
+
+$data = [$user_id,$feed_id];
+$stmt = $dbh->prepare($sql);
+$res = $stmt->execute($data);
+
+echo json_encode($res);
 
 ?>
